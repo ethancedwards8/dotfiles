ccdba9fd0bd46770e121f935d0422ea647e285e8
Ethan Edwards <ethan@ethancedwards.com>
2021-02-10 22:04:58 -0500

    enable doas

--- .nix/nixos-configuration.nix
+++ .nix/nixos-configuration.nix
@@ -75,6 +75,8 @@
     bluetooth.enable = true;
   };
 
+  security.doas.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ece = {
     createHome = true;
