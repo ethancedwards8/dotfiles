ea27120553c582b8e15a3fc97c7ef6b51dbcc3d2
Ethan Edwards <ethan@ethancedwards.com>
2021-02-18 15:41:31 -0500

    adf

--- .nix/nixpkgs/modules/languages-and-server.nix
+++ .nix/nixpkgs/modules/languages-and-server.nix
@@ -12,19 +12,13 @@
     nodePackages.typescript-language-server
 
     rnix-lsp
-<<<<<<< HEAD
-    rust-analyzer rls cargo
-
-
 
     haskell-language-server
-=======
 
     rust-analyzer rls cargo rustc rustfmt
 
     yaml-language-server
 
     nodePackages.vscode-json-languageserver
->>>>>>> 43c8595efe483a21a218b1d0576c5c5b5ea5e57c
   ];
 }
