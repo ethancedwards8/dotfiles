f4a694122c172244a579f1b0b8cd33e76fa564cc
Ethan Edwards <ethan@ethancedwards.com>
2021-04-01 20:02:54 -0400

    elib things

--- .nix/elib/default.nix
+++ .nix/elib/default.nix
@@ -1,11 +1,13 @@
 { self
   , home-manager ? null
   , nixpkgs
+  , darwin
   , inputs
   , pkgs
 }@args:
 let
   lib = nixpkgs.lib;
   nixosSystem = nixpkgs.lib.nixosSystem;
+  darwinSystem = darwin.lib.darwinSystem;
   homeManagerConfiguration = home-manager.lib.homeManagerConfiguration;
-
+in
