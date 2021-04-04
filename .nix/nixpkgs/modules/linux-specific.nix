402a031b97fa28a53d583ce3be492b051dbefe1b
Ethan Edwards <ethan@ethancedwards.com>
2021-04-04 00:22:44 -0400

    add st and dmenu from ethancedwards8-nur

--- .nix/nixpkgs/modules/linux-specific.nix
+++ .nix/nixpkgs/modules/linux-specific.nix
@@ -10,6 +10,7 @@
     monero
     monero-gui
     mpv
+    # nextcloud-client
     nixos-shell
     nodePackages.ganache-cli
     picard
@@ -24,34 +25,9 @@
     xss-lock
     zathura
 
-    # fix bad nextcloud version
-    # (nextcloud-client.overrideAttrs (oldAttrs: rec { 
-    #   version = "3.2.0-rc2";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "nextcloud";
-    #     repo = "desktop";
-    #     rev = "v${version}";
-    #     sha256 = "7euKV6IODa0oMI9LYsr++n9bCHrYzxyzu3KmvzSjvTc=";
-    # };
-    # }))
+    nur.repos.ethancedwards8.st
 
-    (st.overrideAttrs (oldAttrs: rec {
-      src = pkgs.fetchFromGitLab {
-        owner = "ethancedwards";
-        repo = "st-config";
-        rev = "0eedc647ff2c4d19fdaa8c27d4ae0649e44b83e5";
-        sha256 = "oojXFwuYJD9sfbntje9UQ//0bOBtGt0RyFeYwWV7eZo=";
-      };
-    }))
-
-    (dmenu.overrideAttrs (oldAttrs: rec {
-      src = pkgs.fetchFromGitLab {
-        owner = "ethancedwards";
-        repo = "dmenu-config";
-        rev = "9cd6fe49998b48aa1b97e8b66d8895624b0ac897";
-        sha256 = "Kqhf7+kl+izCyrcwW6LaNrbYFh23j1nmBN+7/ebOlA0=";
-      };
-    }))
+    nur.repos.ethancedwards8.dmenu
 
   ];
 
