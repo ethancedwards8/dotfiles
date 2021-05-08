b9489d4c873bccdd76922fa9677a1e264ffb87fe
Ethan Edwards <ethan@ethancedwards.com>
2021-05-08 17:50:42 -0400

    Fix nixlaptop personal modules

--- systems/nixlaptop.nix
+++ systems/nixlaptop.nix
@@ -9,7 +9,36 @@ inputs: {
       home-manager.users."ece" =
         { ... }: {
           imports = [ ../home-manager/modules/linux.nix ];
+
+          ece = {
+            pins = {
+              inherit (inputs)
+                nixpkgs
+                darwin
+
+                home-manager
+                nur;
+            };
+            config = {
+              allowUnfree = true;
+            };
+            overlays = inputs.self.overlays;
+          };
         };
+      ece = {
+        pins = {
+          inherit (inputs)
+            nixpkgs
+            darwin
+
+            home-manager
+            nur;
+        };
+        config = {
+          allowUnfree = true;
+        };
+        overlays = inputs.self.overlays;
+      };
       home-manager.useUserPackages = true;
       home-manager.useGlobalPkgs = true;
       networking.hostName = "nixlaptop";
