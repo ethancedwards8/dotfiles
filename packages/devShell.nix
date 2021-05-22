c8042938a867dde85b5ba036d4420c2c2f49d948
Ethan Edwards <ethan@ethancedwards.com>
2021-05-22 14:25:21 -0400

    fix devShell

--- packages/devShell.nix
+++ packages/devShell.nix
@@ -1,9 +1,13 @@
-{ mkShell, nixpkgs-fmt, git, brave }:
+{ pkgs }:
 
+with pkgs;
 mkShell {
   name = "system-and-home-pull-on-entry";
 
-  buildInputs = [ nixpkgs-fmt git brave /* I want it in my store, but not in my env */ ];
+  # buildInputs = [ nixpkgs-fmt git ];
+  buildInputs = [ nixpkgs-fmt git ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+    brave /* I want it in my store, but not in my env */
+  ];
 
   shellHook = ''
     ${git}/bin/git pull origin master
