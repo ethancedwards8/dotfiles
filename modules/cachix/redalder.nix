0d2fc7e4e41e60f65de3911dc7dc1b0b9b8836b5
Ethan Edwards <ethan@ethancedwards.com>
2021-05-29 09:49:03 -0400

    add hydra.redalder.org to binary caches

---
+++ modules/cachix/redalder.nix
@@ -0,0 +1,11 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://hydra.redalder.org"
+    ];
+    # binaryCachePublicKeys = [
+    #   "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
+    # ];
+  };
+}
+
