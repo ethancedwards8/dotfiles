7796c4218151bb2c36fa244428061e54bcb0462f
Ethan Edwards <ethan@ethancedwards.com>
2021-06-05 10:26:49 -0400

    clean up devShell

--- flake.nix
+++ flake.nix
@@ -89,6 +89,15 @@
 
       darwinPackages = self.darwinConfigurations.mbair.pkgs;
 
-      devShell = forAllSystems (system: nixpkgs.legacyPackages."${system}".callPackage ./packages/devShell.nix { });
-      };
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+          with pkgs; mkShell {
+            name = "system-and-home-pull-on-entry";
+            buildInputs = [ nixpkgs-fmt git ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ brave /* I want it in my store, but not in my env */ ];
+            shellHook = '' ${git}/bin/git pull origin master '';
+          }
+      );
+    };
 }
