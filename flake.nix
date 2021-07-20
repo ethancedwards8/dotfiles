81c4a948891789febc6086f7c66f0bcdc712b2ae
Ethan Edwards <ethan@ethancedwards.com>
2021-07-19 20:33:47 -0400

    fix minor devShell issue

--- flake.nix
+++ flake.nix
@@ -104,7 +104,7 @@
         in
           with pkgs; mkShell {
             name = "system-and-home-pull-on-entry";
-            buildInputs = [ nixpkgs-fmt git ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [ brave /* I want it in my store, but not in my env */ ];
+            buildInputs = [ nixpkgs-fmt git ] ++ pkgs.lib.optionals (pkgs.stdenv.isx86_64 && pkgs.stdenv.isLinux) [ brave /* I want it in my store, but not in my env */ ];
             shellHook = '' ${git}/bin/git pull origin master '';
           }
       );
