e0879837cc2f1bfdc2b0b653b01f30738e898ec4
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-01-29 20:30:59 -0500

    add guix-science

--- .nix/modules/guix.nix
+++ .nix/modules/guix.nix
@@ -7,12 +7,17 @@
     substituters.urls = 
       options.services.guix.substituters.urls.default ++ [
         "https://substitutes.nonguix.org/"
-    ];
+        "https://guix.bordeaux.inria.fr/"
+      ];
     substituters.authorizedKeys =
       options.services.guix.substituters.authorizedKeys.default ++ [
         (builtins.fetchurl {
           url = "https://substitutes.nonguix.org/signing-key.pub";
         })
-    ];
+
+        (builtins.fetchurl {
+          url = "https://guix.bordeaux.inria.fr/signing-key.pub";
+        })
+      ];
   };
 }
