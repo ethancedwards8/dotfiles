df4a88143dcfa9f94d5d8ffab51c867f42d90c76
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-03-23 21:49:20 -0400

    move archpc to packages.nix

--- .nix/systems/archpc.nix
+++ .nix/systems/archpc.nix
@@ -1,18 +1,15 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
+  imports = [
+    ../modules/packages.nix
+  ];
+
   nixpkgs.hostPlatform = "x86_64-linux";
 
   system-manager.allowAnyDistro = true;
 
   environment.systemPackages = with pkgs; [
-    cachix
-    nix-health
-    nix-info
-    nixfmt-rfc-style
-    nix-update
-    nixpkgs-review
-    github-cli
     inputs.system-manager.packages.x86_64-linux.system-manager
   ];
 }
