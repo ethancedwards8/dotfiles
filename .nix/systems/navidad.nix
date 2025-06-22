1e6a06589c948ae65afa914b5465291c6a9edfef
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-22 18:50:30 -0400

    fix hardware for navidad

--- .nix/systems/navidad.nix
+++ .nix/systems/navidad.nix
@@ -11,6 +11,30 @@
     ../modules/xserver.nix
   ];
 
+  ### for Hidpi stuff on framework
+  # bigger tty fonts
+  # console.font =
+  #   "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+  services.xserver.dpi = 180;
+  environment.variables = {
+    ## Used by GTK 3
+    # `GDK_SCALE` is limited to integer values
+    GDK_SCALE = "2";
+    # Inverse of GDK_SCALE
+    GDK_DPI_SCALE = "0.5";
+
+    # Used by Qt 5
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+
+    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  };
+  # Expose variables to graphical systemd user services
+  services.xserver.displayManager.importedVariables = [
+    "GDK_SCALE"
+    "GDK_DPI_SCALE"
+    "QT_AUTO_SCREEN_SCALE_FACTOR"
+  ];
+
   services.tailscale.enable = true;
 
   networking.hostName = "navidad";
