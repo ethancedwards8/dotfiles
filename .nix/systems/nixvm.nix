8febf8e6ba1d4470396cc4e5e4392398f21d9fc3
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-07-03 23:33:18 -0400

    remove impermanence stuff for nixvm, rip

--- .nix/systems/nixvm.nix
+++ .nix/systems/nixvm.nix
@@ -2,8 +2,6 @@
 
 {
   imports = [
-    inputs.impermanence.nixosModules.impermanence
-
     ../hardware/nixvm.nix
 
     ../modules/cachix.nix
@@ -15,24 +13,6 @@
   virtualisation.docker.enable = true;
 
   services.tailscale.enable = true;
-
-  environment.persistence."/nix/persist" = {
-    directories = [
-      "/etc/NetworkManager/system-connections"
-      "/var/log"
-      "/var/lib/systemd/coredump"
-      "/var/lib/nixos"
-      "/var/lib/docker"
-      "/var/lib/tailscale"
-    ];
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-    ];
-  };
 
   networking.hostName = "nixvm";
 
