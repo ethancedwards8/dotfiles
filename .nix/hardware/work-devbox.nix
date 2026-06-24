35e9b652a781fd35418707136b74713e77276a66
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-06-24 17:18:56 -0400

    fix stupid ec2 bs

--- .nix/hardware/work-devbox.nix
+++ .nix/hardware/work-devbox.nix
@@ -5,6 +5,9 @@
     (modulesPath + "/virtualisation/amazon-image.nix")
   ];
 
+  # amazon-image.nix enables amazon-init, which reapplies EC2 user-data on every boot.
+  systemd.services.amazon-init.enable = lib.mkForce false;
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "xfs";
